da8a1f4d9dc9487c231d4739b3d8b2094b0d465a
Deepak Tharanath <deepak.tharanath@johnlewis.co.uk>
2022-06-28 21:03:18 +0100

    Added Terraform required providers

--- challenge1/provider.tf
+++ challenge1/provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.27.0"
+    }
+  }
+}
 provider "google" {
   region  = var.gcp_region
   project = var.gcp_project
