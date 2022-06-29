0ad714fc7ecae076354ba2dd3862aba342e79f85
Deepak Tharanath <deepak.tharanath@johnlewis.co.uk>
2022-06-29 16:21:25 +0100

    Formatted files

--- challenge1/main.tf
+++ challenge1/main.tf
@@ -1,15 +1,15 @@
 module "networking" {
-  source                = "./modules/networking"
+  source = "./modules/networking"
 }
 
 module "instances" {
-  source                = "./modules/instances"
-  private-subnet      = module.networking.private-subnet
-  public-subnet         = module.networking.public-subnet
+  source         = "./modules/instances"
+  private-subnet = module.networking.private-subnet
+  public-subnet  = module.networking.public-subnet
 }
 
 module "database" {
-  source                = "./modules/database"
-  main-vpc-id           = module.networking.main-vpc-id
-  main-vpc-connection   = module.networking.main-vpc-connection
+  source              = "./modules/database"
+  main-vpc-id         = module.networking.main-vpc-id
+  main-vpc-connection = module.networking.main-vpc-connection
 }
